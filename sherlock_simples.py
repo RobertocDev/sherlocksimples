3ec73a4e0ad2d652d295a5afa69a49d25f8f16c7
RobertocDev <54865495+RobertocDev@users.noreply.github.com>
2025-03-10 06:46:51 -0300

    Update sherlock_simples.py

--- sherlock_simples.py
+++ sherlock_simples.py
@@ -5,13 +5,37 @@ app = Flask(__name__)
 
 # Dicionário com os sites que você deseja verificar
 SITES = {
+    "YouTube": "https://www.youtube.com/{}",
+    "Facebook": "https://www.facebook.com/{}",
     "Twitter": "https://twitter.com/{}",
     "Instagram": "https://www.instagram.com/{}",
-    "GitHub": "https://github.com/{}",
+    "TikTok": "https://www.tiktok.com/@{}",
     "Reddit": "https://www.reddit.com/user/{}",
-    "YouTube": "https://www.youtube.com/{}",
-    "Facebook": "https://www.facebook.com/{}"
+    "LinkedIn": "https://www.linkedin.com/in/{}",
+    "Spotify": "https://open.spotify.com/user/{}",
+    "Telegram": "https://t.me/{}",
+    "Twitch": "https://www.twitch.tv/{}",
+    "Pinterest": "https://www.pinterest.com/{}",
+    "GitHub": "https://github.com/{}",
+    "Snapchat": "https://www.snapchat.com/add/{}",
+    "Quora": "https://www.quora.com/profile/{}",
+    "Steam": "https://steamcommunity.com/id/{}",
+    "SoundCloud": "https://soundcloud.com/{}",
+    "Gravatar": "https://www.gravatar.com/{}",
+    "Kwai": "https://www.kwai.com/{}",
+    "Flickr": "https://www.flickr.com/photos/{}",
+    "Medium": "https://medium.com/@{}",
+    "Substack": "https://{}.substack.com",
+    "Trello": "https://trello.com/{}",
+    "Tumblr": "https://tumblr.com{}",
+    "Blogger": "https://{}.blogspot.com",
+    "Dailymotion": "https://www.dailymotion.com/{}",
+    "Catarse": "https://www.catarse.me/{}",
+    "Tenor": "https://tenor.com/users/{}",
+    "Vimeo": "https://vimeo.com/{}"
 }
+
+
 
 @app.route('/check/<username>', methods=['GET'])
 def check_username(username):
